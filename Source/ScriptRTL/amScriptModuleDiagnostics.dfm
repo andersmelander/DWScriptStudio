aef7b6b91fccb96a5b47ff813e6c3389035260c0
Anders Melander <anders@melander.dk>
2023-09-25 21:59:48 +0200

    Added System.Diagnostics script unit. Added TStopWatch class. Incomplete.

---
+++ Source/ScriptRTL/amScriptModuleDiagnostics.dfm
@@ -0,0 +1,108 @@
+object DataModuleScriptDiagnostics: TDataModuleScriptDiagnostics
+  Height = 286
+  Width = 353
+  object dwsUnitSystemStopwatch: TdwsUnit
+    Classes = <
+      item
+        Name = 'TStopWatch'
+        Constructors = <
+          item
+            Name = 'Create'
+            Parameters = <
+              item
+                Name = 'AStart'
+                DataType = 'boolean'
+                HasDefaultValue = True
+                DefaultValue = False
+              end>
+            OnEval = dwsUnitSystemTimersClassesTTimerConstructorsCreateEval
+          end>
+        Methods = <
+          item
+            Name = 'GetElapsed'
+            ResultType = 'Integer'
+            Visibility = cvProtected
+            Kind = mkFunction
+            OnEval = dwsUnitSystemStopwatchClassesTStopWatchMethodsGetElapsedEval
+          end
+          item
+            Name = 'GetElapsedMilliseconds'
+            ResultType = 'Integer'
+            Visibility = cvProtected
+            Kind = mkFunction
+            OnEval = dwsUnitSystemStopwatchClassesTStopWatchMethodsGetElapsedMillisecondsEval
+          end
+          item
+            Name = 'GetElapsedTicks'
+            ResultType = 'Integer'
+            Visibility = cvProtected
+            Kind = mkFunction
+            OnEval = dwsUnitSystemStopwatchClassesTStopWatchMethodsGetElapsedTicksEval
+          end
+          item
+            Name = 'GetIsRunning'
+            ResultType = 'boolean'
+            Visibility = cvProtected
+            Kind = mkFunction
+          end
+          item
+            Name = 'Reset'
+            Kind = mkProcedure
+            OnEval = dwsUnitSystemStopwatchClassesTStopWatchMethodsResetEval
+          end
+          item
+            Name = 'Start'
+            Kind = mkProcedure
+            OnEval = dwsUnitSystemStopwatchClassesTStopWatchMethodsStartEval
+          end
+          item
+            Name = 'Stop'
+            Kind = mkProcedure
+            OnEval = dwsUnitSystemStopwatchClassesTStopWatchMethodsStopEval
+          end
+          item
+            Name = 'StartNew'
+            Kind = mkProcedure
+            OnEval = dwsUnitSystemStopwatchClassesTStopWatchMethodsStartNewEval
+          end
+          item
+            Name = 'IsHighResolution'
+            ResultType = 'boolean'
+            Kind = mkClassFunction
+            OnEval = dwsUnitSystemStopwatchClassesTStopWatchMethodsIsHighResolutionEval
+          end
+          item
+            Name = 'Frequency'
+            ResultType = 'Integer'
+            Kind = mkClassFunction
+            OnEval = dwsUnitSystemStopwatchClassesTStopWatchMethodsFrequencyEval
+          end>
+        Properties = <
+          item
+            Name = 'Elapsed'
+            DataType = 'Integer'
+            ReadAccess = 'GetElapsed'
+          end
+          item
+            Name = 'ElapsedMilliseconds'
+            DataType = 'Integer'
+            ReadAccess = 'GetElapsedMilliseconds'
+          end
+          item
+            Name = 'ElapsedTicks'
+            DataType = 'Integer'
+            ReadAccess = 'GetElapsedTicks'
+          end
+          item
+            Name = 'IsRunning'
+            DataType = 'boolean'
+            ReadAccess = 'GetIsRunning'
+          end>
+        OnCleanUp = dwsUnitSystemTimersClassesTTimerCleanUp
+      end>
+    UnitName = 'System.Diagnostics'
+    StaticSymbols = False
+    Left = 72
+    Top = 32
+  end
+end
---
+++ Source/ScriptRTL/amScriptModuleDiagnostics.pas
@@ -0,0 +1,157 @@
+﻿unit amScriptModuleDiagnostics;
+
+(*
+ * Copyright © 2023 Anders Melander
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *)
+
+interface
+
+uses
+  SysUtils, Classes,
+  Controls,
+  Generics.Collections,
+
+  dwsComp,
+  dwsExprs,
+  dwsCompiler,
+
+  amScriptModule;
+
+// -----------------------------------------------------------------------------
+//
+// TDataModuleSigmaScriptSystem
+//
+// -----------------------------------------------------------------------------
+type
+  TDataModule = class(TScriptModule);
+
+  TDataModuleScriptDiagnostics = class(TDataModule)
+    dwsUnitSystemStopwatch: TdwsUnit;
+    procedure dwsUnitSystemTimersClassesTTimerConstructorsCreateEval(Info: TProgramInfo; var ExtObject: TObject);
+    procedure dwsUnitSystemTimersClassesTTimerCleanUp(ExternalObject: TObject);
+    procedure dwsUnitSystemStopwatchClassesTStopWatchMethodsStartEval(Info: TProgramInfo; ExtObject: TObject);
+    procedure dwsUnitSystemStopwatchClassesTStopWatchMethodsStopEval(Info: TProgramInfo; ExtObject: TObject);
+    procedure dwsUnitSystemStopwatchClassesTStopWatchMethodsResetEval(Info: TProgramInfo; ExtObject: TObject);
+    procedure dwsUnitSystemStopwatchClassesTStopWatchMethodsStartNewEval(Info: TProgramInfo; ExtObject: TObject);
+    procedure dwsUnitSystemStopwatchClassesTStopWatchMethodsGetElapsedMillisecondsEval(Info: TProgramInfo; ExtObject: TObject);
+    procedure dwsUnitSystemStopwatchClassesTStopWatchMethodsGetElapsedTicksEval(Info: TProgramInfo; ExtObject: TObject);
+    procedure dwsUnitSystemStopwatchClassesTStopWatchMethodsIsHighResolutionEval(Info: TProgramInfo; ExtObject: TObject);
+    procedure dwsUnitSystemStopwatchClassesTStopWatchMethodsFrequencyEval(Info: TProgramInfo; ExtObject: TObject);
+    procedure dwsUnitSystemStopwatchClassesTStopWatchMethodsGetElapsedEval(Info: TProgramInfo; ExtObject: TObject);
+  private
+  protected
+  public
+  end;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  ExtCtrls,
+  Variants,
+  DateUtils,
+  Diagnostics,
+
+  dwsMagicExprs,
+  dwsExprList,
+  dwsStrings,
+  dwsSymbols,
+  dwsFunctions,
+
+  amScriptAPI;
+
+// -----------------------------------------------------------------------------
+//
+// Script: TStopwatch
+//
+// -----------------------------------------------------------------------------
+type
+  TScriptStopwatch = class
+  public
+    Stopwatch: TStopwatch;
+  end;
+
+procedure TDataModuleScriptDiagnostics.dwsUnitSystemStopwatchClassesTStopWatchMethodsFrequencyEval(Info: TProgramInfo;
+  ExtObject: TObject);
+begin
+  Info.ResultAsInteger := TStopwatch.Frequency;
+end;
+
+procedure TDataModuleScriptDiagnostics.dwsUnitSystemStopwatchClassesTStopWatchMethodsGetElapsedEval(Info: TProgramInfo;
+  ExtObject: TObject);
+begin
+  Info.ResultAsInteger := 0; // TODO
+end;
+
+procedure TDataModuleScriptDiagnostics.dwsUnitSystemStopwatchClassesTStopWatchMethodsGetElapsedMillisecondsEval(Info: TProgramInfo;
+  ExtObject: TObject);
+begin
+  Info.ResultAsInteger := TScriptStopwatch(ExtObject).Stopwatch.ElapsedMilliseconds;
+end;
+
+procedure TDataModuleScriptDiagnostics.dwsUnitSystemStopwatchClassesTStopWatchMethodsGetElapsedTicksEval(Info: TProgramInfo;
+  ExtObject: TObject);
+begin
+  Info.ResultAsInteger := TScriptStopwatch(ExtObject).Stopwatch.ElapsedTicks;
+end;
+
+procedure TDataModuleScriptDiagnostics.dwsUnitSystemStopwatchClassesTStopWatchMethodsIsHighResolutionEval(Info: TProgramInfo;
+  ExtObject: TObject);
+begin
+  Info.ResultAsBoolean := TStopwatch.IsHighResolution;
+end;
+
+procedure TDataModuleScriptDiagnostics.dwsUnitSystemStopwatchClassesTStopWatchMethodsResetEval(Info: TProgramInfo;
+  ExtObject: TObject);
+begin
+  TScriptStopwatch(ExtObject).Stopwatch.Reset;
+end;
+
+procedure TDataModuleScriptDiagnostics.dwsUnitSystemStopwatchClassesTStopWatchMethodsStartEval(Info: TProgramInfo;
+  ExtObject: TObject);
+begin
+  TScriptStopwatch(ExtObject).Stopwatch.Start;
+end;
+
+procedure TDataModuleScriptDiagnostics.dwsUnitSystemStopwatchClassesTStopWatchMethodsStartNewEval(Info: TProgramInfo;
+  ExtObject: TObject);
+begin
+  TScriptStopwatch(ExtObject).Stopwatch := TStopwatch.StartNew;
+end;
+
+procedure TDataModuleScriptDiagnostics.dwsUnitSystemStopwatchClassesTStopWatchMethodsStopEval(Info: TProgramInfo;
+  ExtObject: TObject);
+begin
+  TScriptStopwatch(ExtObject).Stopwatch.Stop;
+end;
+
+procedure TDataModuleScriptDiagnostics.dwsUnitSystemTimersClassesTTimerCleanUp(ExternalObject: TObject);
+begin
+  ExternalObject.Free;
+end;
+
+procedure TDataModuleScriptDiagnostics.dwsUnitSystemTimersClassesTTimerConstructorsCreateEval(Info: TProgramInfo; var ExtObject: TObject);
+begin
+  if (ExtObject <> nil) then
+    exit;
+
+  ExtObject := TScriptStopwatch.Create;
+
+  if (Info.ParamAsBoolean[0]) then
+    TScriptStopwatch(ExtObject).Stopwatch := TStopwatch.StartNew
+  else
+    TScriptStopwatch(ExtObject).Stopwatch := TStopwatch.Create;
+end;
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+initialization
+  TDataModuleScriptDiagnostics.RegisterModule;
+end.
